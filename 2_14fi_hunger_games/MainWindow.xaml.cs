af7039e3874e0e1acee349ca73e86b34804e1183
jambo <jambo@LAPTOP-TOR02GNI>
2025-03-27 19:39:21 +0100

    OnPropertyChanged megoldással

--- 2_14fi_hunger_games/MainWindow.xaml.cs
+++ 2_14fi_hunger_games/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,33 +19,113 @@ namespace _2_14fi_hunger_games
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         string name = "névtelen";
-        int money = 0;
+        public string Namee
+        {
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+                OnPropertyChanged("Namee");
+            }
+        }
+        int ehsegSzazalek = 100;
+        string kolbaszContent = "Kolbász";
+        int money = 1000;
+        public string KolbaszContent
+        {
+            get
+            {
+                return kolbaszContent;
+            }
+            set
+            {
+                if(value.ToLower() == "csokken")
+                {
+                    kolbaszContent = kolbaszContent.Substring(0, kolbaszContent.Length - 1);
+                }
+                else if(value.ToLower() == "ujratolt")
+                {
+                    kolbaszContent = "Kolbász";
+                }
+                OnPropertyChanged("KolbaszContent");
+            }
+        }
+        public int EhsegSzazalek
+        {
+            get
+            {
+                return ehsegSzazalek;
+            }
+            set
+            {
+                ehsegSzazalek = value;
+
+                if (ehsegSzazalek > 70)
+                    ehseg.Background = new SolidColorBrush(Colors.Red);
+                else if (ehsegSzazalek > 40)
+                    ehseg.Background = new SolidColorBrush(Colors.Orange);
+                else
+                    ehseg.Background = new SolidColorBrush(Colors.Green);
+
+                OnPropertyChanged("EhsegSzazalek");
+            }
+        }
+        public int Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                money = value;
+
+                OnPropertyChanged("Money");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;   //  ChatGPT
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        void NameChange(object s, EventArgs e)
+        {
+            Namee = "Aladár";
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;     // ez kapcsolja össze az xaml fájlunkat, az xaml.cs-sel, így az xaml-ben lévő Binding-ot ott fogja keresni
         }
         void KetchupClick(object s, EventArgs e)
         {
             if (money >= 10)
             {
-                string content = kolbasz.Content.ToString();
-                if (content.Length > 0)
+                //string content = kolbasz.Content.ToString();
+                if (KolbaszContent.Length > 0)
                 {
-                    kolbasz.Content = content.Substring(0, content.Length - 1);
-                    int ehsegSzazalek = int.Parse(ehseg.Content.ToString().Trim('%'));
+                    //kolbasz.Content = content.Substring(0, content.Length - 1);
+                    //int ehsegSzazalek = int.Parse(ehseg.Content.ToString().Trim('%'));
+                    KolbaszContent = "csokken";
                     if (ehsegSzazalek >= 5)
                     {
-                        ehseg.Content = (ehsegSzazalek - 5).ToString() + '%';
+                        EhsegSzazalek -= 5;
+                        //ehseg.Content = (ehsegSzazalek - 5).ToString() + '%';
                     }
-                    EhsegBackground();
-                    money -= 10;
-                    moneyInput.Text = money.ToString();
+                    //EhsegBackground();
+                    //money -= 10;
+                    //moneyInput.Text = money.ToString();
+                    Money -= 10;
                 }
             }
-
         }
         void KolbaszClick(object s, EventArgs e)
         {
@@ -52,17 +133,22 @@ namespace _2_14fi_hunger_games
         }
         void DisznoClick(object s, EventArgs e)
         {
-            kolbasz.Content = "Kolbász";
-            int ehsegSzazalek = int.Parse(ehseg.Content.ToString().Trim('%'));
-            ehseg.Content = (ehsegSzazalek + 20) + "%";
-            if (ehsegSzazalek + 20 > 100)
+            KolbaszContent = "ujratolt";
+            //kolbasz.Content = "Kolbász";
+            //int ehsegSzazalek = int.Parse(ehseg.Content.ToString().Trim('%'));
+            //ehseg.Content = (ehsegSzazalek + 20) + "%";
+            EhsegSzazalek += 20;
+            if (EhsegSzazalek > 100)
             {
                 MessageBox.Show("Éhen haltál!");
                 this.Close();
             }
-            EhsegBackground();
+            //EhsegBackground();
         }
-        void EhsegBackground()
+        //
+        //  Ez a függvény feleslegessé válik, az EhsegSzazalek függvény miatt
+        //
+        /*void EhsegBackground()
         {
             int ehsegSzazalek = int.Parse(ehseg.Content.ToString().Trim('%'));
             if (ehsegSzazalek > 70)
@@ -71,16 +157,17 @@ namespace _2_14fi_hunger_games
                 ehseg.Background = new SolidColorBrush(Colors.Orange);
             else
                 ehseg.Background = new SolidColorBrush(Colors.Green);
-        }
+        }*/
         void ImHungryClick(object s, EventArgs e)
         {
-            name = nevInput.Text;
+            Namee = nevInput.Text;
         }
         void MoneyClick(object s, EventArgs e)
         {
-            money = int.Parse(moneyInput.Text.ToString());
-            moneyInput.IsEnabled = false;
-            moneyButton.Click -= MoneyClick;
+            Money = int.Parse(moneyInput.Text.ToString());
+            //money = int.Parse(moneyInput.Text.ToString());
+            //moneyInput.IsEnabled = false;
+            //moneyButton.Click -= MoneyClick;
         }
     }
 }
